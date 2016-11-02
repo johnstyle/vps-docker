a616a122834ddbb878599cbe3c7fabfddb1a3a09
Jonathan SAHM <contact@johnstyle.fr>
2016-11-02 20:45:20 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Installation globale
+apt-get update
+apt-get install -y fail2ban
+
+# Installation de ZSH
+apt-get install -y git zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Installation de Docker
+apt-get install -y apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-engine
+service docker start
