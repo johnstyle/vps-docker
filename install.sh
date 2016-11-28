9c232020b34ddbec0aaa690dd85c58a00c489535
Jonathan SAHM <contact@johnstyle.fr>
2016-11-28 21:53:28 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,6 @@
 apt-get update
 apt-get install -y fail2ban
 
-# Installation de ZSH
-apt-get install -y git zsh
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
 # Installation de Docker
 apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
@@ -15,3 +11,7 @@ echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sour
 apt-get update
 apt-get install -y docker-engine
 service docker start
+
+# Installation de ZSH
+apt-get install -y git zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
