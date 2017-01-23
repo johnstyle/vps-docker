92a241ff1fccda4772d24a15accec8a10aeff211
Jonathan SAHM <contact@johnstyle.fr>
2017-01-23 21:04:31 +0100

    Update docker-compose install

--- install.sh
+++ install.sh
@@ -9,8 +9,12 @@ apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get install -y docker-engine docker-compose
+apt-get install -y docker-engine
 service docker start
+
+# Installation de Docker Compose
+wget "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -O /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Installation de ZSH
 apt-get install -y git zsh
